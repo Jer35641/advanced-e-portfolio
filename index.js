f568c735b00bc3e18e1304541fff941ab3d2848d
Jer35641 <>
2026-07-09 14:00:39 -0600

    add: feat: background shapes
    add background shapes and the JavaScript to make them move when the mouse moves over them

--- index.js
+++ index.js
@@ -2,15 +2,27 @@
 // HRF8fNargpjD_INU1
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; (i < shapes.length); ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
 
 function toggleContrast() {
-    contrastToggle = !contrastToggle;
-    if (contrastToggle) {
-        document.body.classList += " dark-theme"
-    }
-    else {
-        document.body.classList.remove("dark-theme")
-    }
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
 }
 
 function contact(event) {
@@ -20,28 +32,29 @@ function contact(event) {
   loading.classList += " modal__overlay--visible";
 
   emailjs
-      .sendForm(
-          'service_uctllpc',
-          'template_n9z5mgy',
-          event.target,
-          'HRF8fNargpjD_INU1'
-      ).then(() => {
+    .sendForm(
+      "service_uctllpc",
+      "template_n9z5mgy",
+      event.target,
+      "HRF8fNargpjD_INU1",
+    )
+    .then(() => {
       loading.classList.remove("modal__overlay--visible");
       success.classList += " modal__overlay--visible";
-  }).catch(() => {
-     loading.classList.remove("modal__overlay--visible");
-     alert(
-        "The email service is temporarliy unavailable. Please contact me directly at Jer35643@gmail.com"
-     ); 
-  })
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarliy unavailable. Please contact me directly at Jer35643@gmail.com",
+      );
+    });
 }
 
-
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open")
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open";
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
